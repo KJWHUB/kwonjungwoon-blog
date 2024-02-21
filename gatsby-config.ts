f9e9f41156987449e54b8d707ada50acddd4873d
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-02-21 18:18:52 +0900

    siteMetadata update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `kwon-blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `kwonjungwoon-blog`,
+    siteUrl: `https://kwonjungwoon-blog.netlify.app/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
