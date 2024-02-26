791f52144c01ace307041e0dc8737000855ab005
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-02-26 12:45:29 +0900

    feat: 다크모드 구현

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -1,64 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-// import { Moon, Sun } from 'lucide-react';
-// import SunAndMooon from '../../assets/sun-and-moon.svg';
-import './theme-toggle.js';
+import { Moon, Sun } from 'lucide-react';
+
 import './index.css';
 
-// import useTheme from '../../hooks/useTheme';
+type Theme = 'light' | 'dark';
+
+const getLocalstorageTheme = () => {
+  return localStorage.getItem('theme') as Theme;
+};
+const setLocalstorageTheme = (theme: Theme) => {
+  localStorage.setItem('theme', theme);
+};
+
+const updateThemeOnHtmlEl = (theme: Theme) => {
+  const htmlElement = document.querySelector('html');
+  if (htmlElement) {
+    htmlElement.setAttribute('data-theme', theme);
+  }
+};
+
+const getSystemTheme = (): Theme => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+};
 
 const ThemeToggle = () => {
-  // const { updateTheme, theme } = useTheme();
+  const [theme, setTheme] = useState<Theme>('light');
 
+  const handleClick = () => setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+
+  useEffect(() => {
+    const systemTheme = getSystemTheme();
+    setLocalstorageTheme(systemTheme);
+    setTheme(systemTheme);
+  }, []);
+
+  useEffect(() => {
+    updateThemeOnHtmlEl(theme);
+    setLocalstorageTheme(theme);
+  }, [theme]);
   return (
-    <button
-      className='theme-toggle'
-      id='theme-toggle'
-      title='Toggles light & dark'
-      aria-label='auto'
-      aria-live='polite'
-    >
-      <svg className='sun-and-moon' aria-hidden='true' width='24' height='24' viewBox='0 0 24 24'>
-        <mask className='moon' id='moon-mask'>
-          <rect x='0' y='0' width='100%' height='100%' fill='white' />
-          <circle cx='24' cy='10' r='6' fill='black' />
-        </mask>
-        <circle className='sun' cx='12' cy='12' r='6' mask='url(#moon-mask)' fill='currentColor' />
-        <g className='sun-beams' stroke='currentColor'>
-          <line x1='12' y1='1' x2='12' y2='3' />
-          <line x1='12' y1='21' x2='12' y2='23' />
-          <line x1='4.22' y1='4.22' x2='5.64' y2='5.64' />
-          <line x1='18.36' y1='18.36' x2='19.78' y2='19.78' />
-          <line x1='1' y1='12' x2='3' y2='12' />
-          <line x1='21' y1='12' x2='23' y2='12' />
-          <line x1='4.22' y1='19.78' x2='5.64' y2='18.36' />
-          <line x1='18.36' y1='5.64' x2='19.78' y2='4.22' />
-        </g>
-      </svg>
+    <button title='Toggles light & dark' onClick={handleClick} className='theme-toggle'>
+      {theme === 'light' ? <Sun strokeWidth={1.75} /> : <Moon strokeWidth={1.75} />}
     </button>
   );
-
-  // if (theme === 'dark') {
-  //   return (
-  //     <Moon
-  //       size={50}
-  //       strokeWidth={1.75}
-  //       onClick={() => {
-  //         updateTheme('light');
-  //       }}
-  //     />
-  //   );
-  // }
-
-  // return (
-  //   <Sun
-  //     size={50}
-  //     strokeWidth={1.75}
-  //     onClick={() => {
-  //       updateTheme('dark');
-  //     }}
-  //   />
-  // );
 };
 
 export default ThemeToggle;
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,30 +1,22 @@
-import React, { createContext, useState } from 'react';
+import React from 'react';
 import '../styles/index.css';
 
 import { container } from './style.module.scss';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import useTheme, { getSystemTheme } from '../hooks/useTheme';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
-const ThemeContext = createContext('light');
-
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const { setTheme, theme, setLocalstorageTheme } = useTheme();
-
-  setLocalstorageTheme(getSystemTheme());
   return (
-    <ThemeContext.Provider value={theme}>
-      <div>
-        <Header />
-        <main className={container}>{children}</main>
-        <Footer />
-      </div>
-    </ThemeContext.Provider>
+    <div>
+      <Header />
+      <main className={container}>{children}</main>
+      <Footer />
+    </div>
   );
 };
 
