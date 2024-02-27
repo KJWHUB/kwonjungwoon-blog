24ffd5c75cadb33b2e4a16b1443f8e6ab8c9a5d7
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-02-27 19:02:15 +0900

    feat: nav setup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,31 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
-import { header } from './style.module.scss';
 import ThemeToggle from '../ThemeToggle';
+import { header } from './style.module.scss';
 
 const Logo = () => {
   return <h1>Logo</h1>;
 };
 
+const NavigationWrap = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>
+            <Logo />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 const Header = () => {
   return (
     <header className={header}>
-      <Logo />
+      <NavigationWrap />
       <ThemeToggle />
     </header>
   );
