a4d3b7ef204e265c57695d610af0daee09267eee
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-03-06 11:33:29 +0900

    gatsby-node.ts와 blog-post.tsx에 변경 내용 추가

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,5 +1,60 @@
-import type { GatsbyNode } from 'gatsby';
+import type { Actions, GatsbyNode } from 'gatsby';
 import { createFilePath } from 'gatsby-source-filesystem';
+import path from 'path';
+
+type qCPQ = Queries.CreatePostQuery;
+
+const createPost = ({
+  createPage,
+  edges,
+}: {
+  createPage: Actions['createPage'];
+  edges: qCPQ['allMarkdownRemark']['edges'];
+}) => {
+  const post = path.resolve(`./src/templates/blog-post.tsx`);
+
+  edges.forEach(({ node }) => {
+    const slug = node?.fields?.slug;
+    createPage({
+      path: `/post${slug}`,
+      component: post,
+      context: {
+        slug,
+      },
+    });
+  });
+};
+
+export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql<qCPQ>(`
+    query CreatePost {
+      allMarkdownRemark(sort: { frontmatter: { date: DESC } }, limit: 1000) {
+        edges {
+          node {
+            id
+            excerpt(pruneLength: 500, truncate: true)
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              date(formatString: "YYYY.MM.DD")
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors || !result.data) {
+    reporter.panicOnBuild(`There was an error loading your blog posts`, result.errors);
+    return;
+  }
+
+  createPost({ createPage, edges: result.data.allMarkdownRemark.edges });
+};
 
 export const onCreateNode: GatsbyNode['onCreateNode'] = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,7 +1,26 @@
+import { graphql, type PageProps } from 'gatsby';
 import React from 'react';
 
-const BlogPostTemplate = () => {
+const BlogPostTemplate = ({ data }: PageProps<Queries.PostDetailQuery>) => {
+  console.log('data ;;;', data);
   return <div>Hello blog post</div>;
 };
+
+export const query = graphql`
+  query PostDetail($slug: String) {
+    cur: markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      excerpt(pruneLength: 500, truncate: true)
+      frontmatter {
+        date(formatString: "YYYY.MM.DD")
+        title
+      }
+      fields {
+        slug
+      }
+    }
+  }
+`;
 
 export default BlogPostTemplate;
