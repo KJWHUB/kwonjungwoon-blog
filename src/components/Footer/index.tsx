7bc4a790f4de4c09fbb45f88a0e844ff40fc0474
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-03-08 15:58:35 +0900

    footer 포지션 변경

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { footer } from './style.module.scss';
+import { classNames } from '@/src/utils/className';
+
+import { footer, scrollbarFalse, scrollbarTrue } from './style.module.scss';
 
 const Footer = () => {
+  const [isScrollBar, setIsScrollBar] = useState(true);
+
+  useEffect(() => {
+    // 문서 전체의 높이
+    const documentHeight = document.body.scrollHeight;
+
+    // 브라우저 창의 높이
+    const windowHeight = window.innerHeight;
+
+    // 문서 전체의 높이가 브라우저 창의 높이보다 클 경우 스크롤바가 존재함
+    const isScrollBarExist = documentHeight > windowHeight;
+
+    if (!isScrollBarExist) setIsScrollBar(false);
+  }, []);
+
   return (
-    <footer className={footer}>
+    <footer className={classNames(footer, isScrollBar ? scrollbarTrue : scrollbarFalse)}>
       <p>© 2024 Kwon Jungwoon. All rights reserved.</p>
     </footer>
   );
